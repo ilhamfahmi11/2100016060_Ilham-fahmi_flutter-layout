1c5836380cc621001d712ee030916d60c910a26f
ilhamfahmi11 <ilham.fahmi011@gmail.com>
2024-04-01 13:12:52 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text("Stack"),
+          ),
+          body: Center(
+            child: Stack(
+              children: <Widget>[
+                Align(
+                    alignment: Alignment.center,
+                    child: Container(
+                      color: Colors.red,
+                      height: 100,
+                      width: 100,
+                    )),
+                Align(
+                    alignment: Alignment.center,
+                    child: Container(
+                      color: Colors.blue,
+                      height: 100,
+                      width: 80,
+                      margin: EdgeInsets.only(top: 20),
+                    )),
+                Align(
+                    alignment: Alignment.center,
+                    child: Container(
+                      color: Colors.purple,
+                      height: 70,
+                      width: 70,
+                      margin: EdgeInsets.only(top: 20),
+                    ))
+              ],
+            ),
+          )),
+    );
+  }
+}
